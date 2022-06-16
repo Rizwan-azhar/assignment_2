762d50be0a878a6541803bcbf9984c53c2b946ec
Rizwan-azhar <mrazhar014@gmail.com>
2022-06-16 22:28:15 +0500

    yp

---
+++ myportfolio/myportfolio/index.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render 
+
+def index(request):
+    return render(request, 'index.html')
+
+    
+def contact(request):
+    return render(request, 'contact.html')
+
+    
+def projects(request):
+    return render(request, 'projects.html')
+
+    
+def resume(request):
+    return render(request, 'resume.html')
